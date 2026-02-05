c96a7d5e0c94315d5bd350d6bacf72128245b416
Josef Hofer <me@jhofer.de>
2026-02-05 12:38:18 +0100

    fix(mod/sh/git): git release_notes alias (#332)
    Co-authored-by: Josef Hofer <josef.hofer@1und1.de>

--- modules/shared/home/git/default.nix
+++ modules/shared/home/git/default.nix
@@ -49,7 +49,7 @@
           ll = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate --numstat";
           ls = "log --pretty=format:\"%C(yellow)%h%Cred%d\\\\ %Creset%s%Cblue\\\\ [%cn]\" --decorate";
           mr = "!sh -c 'git fetch $1 merge-requests/$2/head:mr-$1-$2 && git checkout mr-$1-$2' -";
-          release_notes = "!f() { git log'$1..main' --no-merges --pretty=format:'* %s'; }; f";
+          release-notes = "!f() { git log \"$1..main\" --no-merges --pretty=format:'* %s'; }; f";
           root = "rev-parse --show-toplevel";
           st = "status";
           update = "submodule update --init --recursive";
