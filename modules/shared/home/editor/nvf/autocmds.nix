a5d7f93ceaa87ce2e483b2e6e45a66eb2584fbe1
Josef Hofer <me@jhofer.de>
2026-02-12 15:14:49 +0100

    fix/opencode editor (#423)
    * Revert "fix(mod/sh/editor+opencode): use tabs for sh/bash files (dont expand tabs) (#422)"
    
    This reverts commit 58b6ba710240ce6e9a4ef28c5cc94b7d54efd7a6.
    
    * fix(mod/sh/opencode): use 2 for shfmt
    
    Co-authored-by: Josef Hofer <josef.hofer@1und1.de>
    
    ---------
    
    Co-authored-by: Josef Hofer <josef.hofer@1und1.de>

--- modules/shared/home/editor/nvf/autocmds.nix
+++ modules/shared/home/editor/nvf/autocmds.nix
@@ -7,16 +7,6 @@
 in {
   # Autocmds for filetype-specific settings
   autocmds = [
-    # Use tabs instead of spaces
-    {
-      event = ["FileType"];
-      pattern = ["sh" "bash"];
-      callback = lib.generators.mkLuaInline ''
-        function()
-          vim.opt_local.expandtab = false
-        end
-      '';
-    }
     # Indent-based folding for specific filetypes
     {
       event = ["FileType"];
@@ -30,7 +20,7 @@ in {
     # Smaller indentation for specific filetypes
     {
       event = ["FileType"];
-      pattern = ["helm" "nix" "json" "jsonc" "json5"];
+      pattern = ["sh" "bash" "helm" "nix" "json" "jsonc" "json5"];
       callback = lib.generators.mkLuaInline ''
         function()
           vim.opt_local.tabstop = 2
