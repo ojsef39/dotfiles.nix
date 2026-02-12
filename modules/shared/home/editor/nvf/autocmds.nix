60a729df84a5d3aa6878473f6b5681c6f986ae0e
Josef Hofer <josef.hofer@1und1.de>
2026-02-12 12:14:08 +0100

    fix(mod/sh/editor+opencode): use tabs for sh/bash files (dont expand tabs)

--- modules/shared/home/editor/nvf/autocmds.nix
+++ modules/shared/home/editor/nvf/autocmds.nix
@@ -7,6 +7,16 @@
 in {
   # Autocmds for filetype-specific settings
   autocmds = [
+    # Use tabs instead of spaces
+    {
+      event = ["FileType"];
+      pattern = ["sh" "bash"];
+      callback = lib.generators.mkLuaInline ''
+        function()
+          vim.opt_local.expandtab = false
+        end
+      '';
+    }
     # Indent-based folding for specific filetypes
     {
       event = ["FileType"];
