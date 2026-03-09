4d693e562f557fa5528c556dc2ccc5bd7614a03e
Josef Hofer <me@jhofer.de>
2026-03-09 10:20:48 +0100

    feat(mod/dw/nixkit): add nixupter menu bar

--- modules/darwin/home/nixkit/default.nix
+++ modules/darwin/home/nixkit/default.nix
@@ -1,6 +1,15 @@
-_: {
-  programs.default-browser = {
-    enable = true;
-    browser = "zen"; # browser = Arc.
+{vars, ...}: let
+  dotPath = "/Users/${vars.user.name}/${vars.git.ghq}/github.com/ojsef39/dotfiles.nix";
+in {
+  programs = {
+    default-browser = {
+      enable = true;
+      browser = "zen"; # browser = Arc.
+    };
+    nixupdater = {
+      enable = true;
+      flake = dotPath;
+      command = "NIX_GIT_PATH=${dotPath} just upgrade";
+    };
   };
 }
