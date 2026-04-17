fc73f0238c3e237d57b6f0f4f2ba86a74d52a04d
Josef Hofer <josef.hofer@1und1.de>
2026-04-17 19:24:35 +0200

    feat(mod/sh/ai): fix context7 name
    Co-authored-by: Josef Hofer <josef.hofer@1und1.de>

--- modules/shared/home/ai/mcp.nix
+++ modules/shared/home/ai/mcp.nix
@@ -1,11 +1,15 @@
-{pkgs, ...}: {
+{
+  pkgs,
+  lib,
+  ...
+}: {
   programs.mcp = {
     enable = true;
     servers = {
-      context7 = {
+      "io.github.upstash/context7" = {
         command = "${pkgs._1password-cli}/bin/op";
         args = ["run" "--" "${pkgs.context7-mcp}/bin/context7-mcp"];
-        env.CONTEXT7_API_KEY = "op://Personal/Context7/api_key";
+        env.CONTEXT7_API_KEY = lib.mkDefault "op://Personal/Context7/api_key";
       };
     };
   };
